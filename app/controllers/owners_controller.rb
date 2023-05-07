b47cbaaf745896e82f2e1c4ac4a67799146768cf
Vladyslav <vkadyrov@protonmail.com>
2023-05-07 17:20:47 -0400

    Routes for Owners Controller have been added

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,2 +1,52 @@
 class OwnersController < ApplicationController
+  before_action :set_owner, only: %i[show update destroy]
+
+  def show
+    render json: owner
+  end
+
+  def update
+    if owner.update(owner_params)
+      render json: owner
+    else
+      render json: {
+               errors: owner.errors.full_messages,
+             },
+             status: :unprocessable_entity
+    end
+  end
+
+  def create
+    owner = Owner.new(owner_params)
+    if owner.save
+      render json: owner, status: :created
+    else
+      render json: {
+               errors: owner.errors.full_messages,
+             },
+             status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    owner.destroy
+    render json: { message: 'Owner deleted' }, status: :no_content
+  end
+
+  private
+
+  def set_owner
+    owner = Owner.fint_by(id: params[:id])
+
+    render json: { error: 'Owner not found' }, status: :not_found unless owner
+  end
+
+  def owner_params
+    params.require(:owner).permit(
+      :name,
+      :email,
+      :password,
+      :password_confirmation,
+    )
+  end
 end
