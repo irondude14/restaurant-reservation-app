b8903af78e87442c736e4081a3f8494d86c17791
Vladyslav <vkadyrov@protonmail.com>
2023-05-07 20:03:08 -0400

    User Model & Controller were updated, show method was added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show update destroy]
+
+  def show
+    render json: @user, include: :reservations
+  end
+
+  private
+
+  def set_user
+    @user = User.includes(:reservations).find_by(id: params[:id])
+
+    render json: { error: 'User not found' }, status: :not_found unless @user
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :email,
+      :password,
+      :phone,
+      :password_confirmation,
+    )
+  end
 end
