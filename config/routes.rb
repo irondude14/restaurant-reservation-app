22dde431ab234becdb82367450e0838f82441e44
Vladyslav <vkadyrov@protonmail.com>
2023-05-15 20:31:33 -0400

    Routes were updated to include /sessions for users

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :owners
   resources :reservations
   resources :users
+
+  post '/sessions', to: 'users#create_session'
+  delete '/sessions', to: 'users#destroy_session'
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get '*path',
