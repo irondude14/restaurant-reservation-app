1fefa698707dda0ad128619202175eda91cc8423
Vladyslav <vkadyrov@protonmail.com>
2023-05-25 21:25:27 -0400

    More changes to the UsersPage

--- client/src/components/UsersPage.js
+++ client/src/components/UsersPage.js
@@ -28,87 +28,88 @@ function UsersPage() {
 
   console.log(user);
 
-  // if (
-  //   user &&
-  //   user.reservations &&
-  //   user.reservations.length > 0 &&
-  //   user.restaurants
-  // ) {
-  return (
-    <div>
-      <h3>Your reservations:</h3>
-      <ul>
-        {user.reservations.map((reservation) => {
-          const { restaurant } = reservation;
+  if (
+    user
+    // &&
+    // user.reservations &&
+    // user.reservations.length > 0 &&
+    // user.restaurants
+  ) {
+    return (
+      <div>
+        <h3>Your reservations:</h3>
+        <ul>
+          {user.reservations.map((reservation) => {
+            const { restaurant } = reservation;
 
-          return (
-            <li key={reservation.id}>
-              <p>Name on a reservation: {reservation.name}</p>
-              <p>
-                Reservation time:{' '}
-                {new Date(reservation.date_time).toLocaleDateString('en-US', {
-                  year: 'numeric',
-                  month: 'long',
-                  day: 'numeric',
-                  hour: 'numeric',
-                  minute: 'numeric',
-                })}
-              </p>
-              <p>Number of guests: {reservation.guest_number}</p>
-              {restaurant && (
-                <div>
-                  <p>Restaurant: {restaurant.name}</p>
-                  <img src={restaurant.image_url} alt={restaurant.name} />
-                  <p>Address: {restaurant.address}</p>
-                  <p>Phone: {restaurant.phone}</p>
-                </div>
-              )}
-              <button>Edit</button>
-              <button>Delete</button>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
-  // } else if (user && user.reservations && user.reservations.length === 0) {
-  //   return <div>Here will be your reservations.</div>;
-  // } else if (user && user.restaurants && user.restaurants.length > 0) {
-  //   return (
-  //     <div>
-  //       <h3>Your restaurants:</h3>
-  //       <ul>
-  //         {user.restaurants.map((rest) => {
-  //           return (
-  //             <li key={rest.id}>
-  //               <h3>{rest.name}</h3>
-  //               <p>{rest.description}</p>
-  //               <img src={rest.image_url} alt={rest.name} />
-  //               <p>Address: {rest.address}</p>
-  //               <p>Phone: {rest.phone}</p>
-  //               <button>Edit</button>
-  //               <button>Delete</button>
-  //             </li>
-  //           );
-  //         })}
-  //       </ul>
-  //       <button>Add a Restaurant</button>
-  //     </div>
-  //   );
-  // } else if (user && user.restaurants && user.restaurants.length === 0) {
-  //   return (
-  //     <div>
-  //       <h3>Here will be your restaurants.</h3>
-  //       <button>Add a Restaurant</button>
-  //     </div>
-  //   );
-  // } else {
-  //   return (
-  //     <div>
-  //       <h3>Something went wrong. Please contact support.</h3>
-  //     </div>
-  //   );
-  // }
+            return (
+              <li key={reservation.id}>
+                <p>Name on a reservation: {reservation.name}</p>
+                <p>
+                  Reservation time:{' '}
+                  {new Date(reservation.date_time).toLocaleDateString('en-US', {
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric',
+                    hour: 'numeric',
+                    minute: 'numeric',
+                  })}
+                </p>
+                <p>Number of guests: {reservation.guest_number}</p>
+                {restaurant && (
+                  <div>
+                    <p>Restaurant: {restaurant.name}</p>
+                    <img src={restaurant.image_url} alt={restaurant.name} />
+                    <p>Address: {restaurant.address}</p>
+                    <p>Phone: {restaurant.phone}</p>
+                  </div>
+                )}
+                <button>Edit</button>
+                <button>Delete</button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  } else if (user && user.reservations.length === 0) {
+    return <div>Here will be your reservations.</div>;
+    // } else if (user && user.restaurants && user.restaurants.length > 0) {
+    //   return (
+    //     <div>
+    //       <h3>Your restaurants:</h3>
+    //       <ul>
+    //         {user.restaurants.map((rest) => {
+    //           return (
+    //             <li key={rest.id}>
+    //               <h3>{rest.name}</h3>
+    //               <p>{rest.description}</p>
+    //               <img src={rest.image_url} alt={rest.name} />
+    //               <p>Address: {rest.address}</p>
+    //               <p>Phone: {rest.phone}</p>
+    //               <button>Edit</button>
+    //               <button>Delete</button>
+    //             </li>
+    //           );
+    //         })}
+    //       </ul>
+    //       <button>Add a Restaurant</button>
+    //     </div>
+    //   );
+    // } else if (user && user.restaurants && user.restaurants.length === 0) {
+    //   return (
+    //     <div>
+    //       <h3>Here will be your restaurants.</h3>
+    //       <button>Add a Restaurant</button>
+    //     </div>
+    //   );
+  } else {
+    return (
+      <div>
+        <h3>Something went wrong. Please contact support.</h3>
+      </div>
+    );
+  }
 }
 
 export default UsersPage;
