9020731762384d8f859cc978bd2ca285fd4123ba
Vladyslav <vkadyrov@protonmail.com>
2023-06-13 11:44:33 -0400

    Cleaning some notes

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -2,18 +2,15 @@ class OwnershipsController < ApplicationController
   before_action :set_ownership, only: %i[show update destroy]
   before_action :authorize_ownership, only: %i[show update destroy]
 
-  # GET /ownerships
   def index
     @ownerships = Ownership.all
     render json: @ownerships
   end
 
-  # GET /ownerships/1
   def show
     render json: @ownership
   end
 
-  # POST /ownerships
   def create
     @ownership = Ownership.new(ownership_params)
 
@@ -24,7 +21,6 @@ class OwnershipsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /ownerships/1
   def update
     if @ownership.update(ownership_params)
       render json: @ownership
@@ -33,14 +29,12 @@ class OwnershipsController < ApplicationController
     end
   end
 
-  # DELETE /ownerships/1
   def destroy
     @ownership.destroy
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_ownership
     @ownership = Ownership.find(params[:id])
   end
@@ -62,7 +56,6 @@ class OwnershipsController < ApplicationController
     render json: { error: 'Not Authorized' }, status: :unauthorized
   end
 
-  # Only allow a list of trusted parameters through.
   def ownership_params
     params.require(:ownership).permit(:user_id, :restaurant_id)
   end
--- app/models/owner.rb
+++
@@ -1,7 +0,0 @@
-class Owner < ApplicationRecord
-  has_secure_password
-  validates :name, :password, presence: true
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-
-  has_many :restaurants
-end
