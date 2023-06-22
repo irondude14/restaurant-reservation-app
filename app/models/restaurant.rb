a515243728cfeefef4801e4541807f381f12dc18
Vladyslav <vkadyrov@protonmail.com>
2023-06-22 15:09:33 -0400

    Updated validation for Reservations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,11 +1,23 @@
 class Restaurant < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
-  validates :phone, presence: true, format: { with: /\A\+?\d+\z/, message: "only allows numbers" }
-  validates :price, presence: true, numericality: { only_integer: true, message: "only allows whole numbers" }
+  validates :phone,
+            presence: true,
+            format: {
+              with: /\A\+?\d+\z/,
+              message: 'only allows numbers',
+            }
+  validates :price,
+            presence: true,
+            numericality: {
+              only_integer: true,
+              greater_than_or_equal_to: 1,
+              less_than_or_equal_to: 5,
+              message: 'only allows whole numbers',
+            }
   validates :image_url, presence: true
   validates :description, presence: true
-  
+
   has_many :ownerships
   has_many :owners, through: :ownerships, source: :user
   has_many :reservations, dependent: :destroy
